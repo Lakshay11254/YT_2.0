65e89c5b12dfc517728fe6cbfeacce95106dfe8b
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-01-12 00:53:52 +0530

    front page done

---
+++ src/components/Feed.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
+
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+<SideBar/>  
+      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2023 Lakshay Malhotra
+        </Typography>
+
+  </Box>
+  </Stack>
+  )
+}
+
+export default Feed
