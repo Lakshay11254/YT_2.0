5bb15b89a13a4d78595700c3864eab3d4466ca98
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-01-12 01:00:18 +0530

    ..

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,6 +13,13 @@ const Feed = () => {
         </Typography>
 
   </Box>
+  <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          New <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        {/* <Videos videos={videos} /> */}
+      </Box>
   </Stack>
   )
 }
