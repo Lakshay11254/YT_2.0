1b290910550de38842ad4061ae349fa79542e7d2
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-01-13 00:13:19 +0530

    videocard  done now channel card time

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,7 +8,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   // console.log(videoId,snippet);
   return (
-    <Card>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
       <Link to={videoId ? `/video/${videoId}`
        : demoVideoUrl}>
       <CardMedia 
@@ -17,6 +17,21 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
       sx={{width:358,height:180}}
       />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        </Typography>
+      </Link>
+
+        </CardContent>
       </Card>
   )
 }
