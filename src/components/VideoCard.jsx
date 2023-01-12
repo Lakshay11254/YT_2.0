1bb71a25f4880d4d455cc4a22c76eddbded8f4a6
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-01-13 00:02:38 +0530

    we gotaa d thumbnails

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from "react-router-dom"; 
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const VideoCard = () => {
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+  // console.log(videoId,snippet);
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}`
+       : demoVideoUrl}>
+      <CardMedia 
+      image={snippet?.thumbnails?.high?.url}
+      alt={snippet?.title}
+      sx={{width:358,height:180}}
+      />
+      </Link>
+      </Card>
   )
 }
 
