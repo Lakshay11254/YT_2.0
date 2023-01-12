e6e257cb698383bbcf776d5fcd232024e2d97a94
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-01-12 23:32:37 +0530

    ..

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import { Stack, Box } from "@mui/material";
-import ChannelCard from "./ChannelCard";
-import VideoCard from "./VideoCard";
+import React from 'react'
 
-const Videos = ({ videos}) => {
-  // if(!videos?.length) return <Loader />;
-  
+const Videos = () => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
-        </Box>
-      ))}
-    </Stack>
-  );
+    <div>Videos</div>
+  )
 }
+
 export default Videos
